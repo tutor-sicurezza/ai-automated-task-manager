87bd56745f4f403bd58d04123cc44370bdc0efc5
tutor-sicurezza <tutorsicurezza@gmail.com>
2026-05-12 22:25:34 +0000

    Edited Spark

--- src/components/EmailDigestSystem.tsx
+++ src/components/EmailDigestSystem.tsx
@@ -1,27 +1,27 @@
 import { Button } from '@/components/ui/button';
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
 import { Alert, AlertDescription } from '@/components/ui/alert';
-import { Envelope } from '@phosphor-icons/react';
-
-export function EmailDigestSystem() {
+import { Alert, AlertDescription } from '@/components/ui/alert';
   return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <Button variant="outline">
-          <Envelope className="mr-2 h-4 w-4" weight="bold" />
-          Email Digests
+
+          <Envelope className="mr-2 h
+  return (
+      <Dialo
+          <DialogTitle>Email 
+        <Alert>
+            Email digest system is temporarily disabled. This
+        </Alert>
         </Button>
-      </DialogTrigger>
-      <DialogContent className="max-w-2xl">
-        <DialogHeader>
+}
+
+
           <DialogTitle>Email Digest System</DialogTitle>
         </DialogHeader>
-        <Alert>
-          <AlertDescription>
-            Email digest system is temporarily disabled. This feature will be rebuilt soon with improved functionality.
-          </AlertDescription>
-        </Alert>
+
+
+
+
+
       </DialogContent>
     </Dialog>
-  );
+
 }
